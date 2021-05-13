d495093ebe40474ce07a3282a0f451eadf3f539c
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2021-05-13 14:00:45 +0200

    echo async

---
+++ server/echo_async_server.py
@@ -0,0 +1,17 @@
+import asyncio
+
+async def echo_server(address):
+    server = await asyncio.start_server(echo_handler, *address)
+    await server.serve_forever()
+
+async def echo_handler(reader, writer):
+    while True:
+        data = await reader.read(100000)
+        if not data:
+            break
+        writer.write(b'reply: ' + data)
+        await writer.drain()
+    print("conn closed")
+    await writer.wait_close()
+
+asyncio.run(echo_server(("", 25000)))
