820f2e6ef4a4c3d32de8f302b22dc8367b62b8ee
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2021-05-14 10:53:36 +0200

    file CM - more tests

--- async_file_manager.py
+++ async_file_manager.py
@@ -1,12 +1,22 @@
 import asyncio
 
 class AsyncFile:
-    pass
+    def __init__(self, filename):
+        self.filename = filename
 
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, ext, exc, tb):
+        pass
+
+    async def read(self):
+        return None
 
 async def main():
     async with AsyncFile("hello_async.py") as f:
         content = await f.read()
     print(content)
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
--- test_async_file_manager.py
+++ test_async_file_manager.py
@@ -1,3 +1,4 @@
+import inspect
 import pytest
 import asyncio
 import contextlib
@@ -5,8 +6,24 @@ import unittest.mock as mock
 
 from async_file_manager import AsyncFile
 
+lines = ["abc\r\n", "def\r\n", "ghi\r\n"]
+content = ''.join(lines)
+
 @pytest.mark.asyncio
 async def test_AsyncFileCM():
     with mock.patch('builtins.open', mock.mock_open()):
         async with AsyncFile("test_file") as f:
             assert isinstance(f, contextlib.AbstractAsyncContextManager)
+
+@pytest.mark.asyncio
+async def test_AsyncFileCM_read_coro():
+    with mock.patch('builtins.open', mock.mock_open()):
+        async with AsyncFile("test_file") as f:
+            assert asyncio.iscoroutinefunction(f.read)
+
+@pytest.mark.asyncio
+async def test_AsyncFileCM_read_coro():
+    with mock.patch('builtins.open', mock.mock_open(read_data=content)):
+        async with AsyncFile("test_file") as f:
+            data = await f.read()
+            assert data == content
