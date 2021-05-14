7296ed66fa4796a1150b18e18bc350a806e22d34
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2021-05-14 11:58:10 +0200

    dir search

--- dir_search.py
+++ dir_search.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import time
 from async_file_manager import AsyncFile
 
 async def get_python_filenames(startdir):
@@ -8,9 +9,22 @@ async def get_python_filenames(startdir):
             if filename.endswith(".py"):
                 yield os.path.join(dir, filename)
 
+def process_file(filename):
+    counter = 0
+    with open(filename) as f:
+        for _ in f:
+            counter += 1
+    return counter
 
 async def main():
+    counter = 0
     async for f in get_python_filenames("."):
-        print(f)
+        counter += process_file(f)
+    return counter
 
-asyncio.run(main())
+start = time.time()
+res = asyncio.run(main())
+end = time.time()
+
+print(f"time: {end-start}")
+print(f"res = {res}")
